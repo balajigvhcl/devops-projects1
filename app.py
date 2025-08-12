cba7caef50c3771170c3ca877324ad4c4917c5b7
balaji <balajigv.hcl@gmail.com>
2025-08-12 11:32:16 +0530

    first

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/info")
+def lwinfo():
+	return "iam balaji-gv from scb"
+
+@app.route("/phone")
+def lwphone():
+	return "9949095520"
+
+app.run(host="0.0.0.0")
